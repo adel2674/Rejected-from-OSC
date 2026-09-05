b38058642aed8d6b3e2cffbc5ec9bd0dc27eff82
Abdelrahman Medhat <AboodisGit@gmail.com>
2026-09-05 04:17:31 +0300

    Add basic throwers and random anlges

---
+++ game-jam/Scripts/thrower.gd
@@ -0,0 +1,25 @@
+
+extends CharacterBody2D
+@onready var ball: RigidBody2D = $"../RigidBody2D"
+@onready var ani: AnimatedSprite2D = $AnimatedSprite2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+
+@export var target_character: CharacterBody2D
+
+func throw():
+		ball.throw(target_character.global_position)
+		ani.play("throw")
+		await ani.animation_finished
+		ani.play("default")
+		
+		
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("ball"):
+		throw()
+		
+		
+	
