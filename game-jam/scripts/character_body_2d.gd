a414431228f3c301b957cc8ef5c0dddde1b35be2
zizozexo369 <ziad2007mahmoud@gmail.com>
2026-09-06 23:16:27 +0300

    added sounds and catching

--- game-jam/scripts/character_body_2d.gd
+++ game-jam/scripts/character_body_2d.gd
@@ -1,47 +1,111 @@
 extends CharacterBody2D
 
-const SPEED = 150.0
-const JUMP_VELOCITY = -300.0
+const SPEED = 450.0
+const JUMP_VELOCITY = -600.0
+var can_catch := true
 
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var idle_coll: CollisionShape2D = $idle_coll
 @onready var move_coll: CollisionShape2D = $move_coll
+@onready var jump_sfx: AudioStreamPlayer2D = $jump_sfx
+@onready var landing_sfx: AudioStreamPlayer2D = $landing_sfx
+@onready var catch_sfx: AudioStreamPlayer2D = $catch_sfx
+@onready var catch_cooldown: Timer = $catch_cooldown
+
+@onready var hitbox_move: Area2D = $hitbox_move
+@onready var hitbox_idle: Area2D = $hitbox_idle
+var held_ball: RigidBody2D = null
+var was_in_air = false
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# 1. Gravity and Jump Input
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-		animated_sprite_2d.play("jump")
-		idle_coll.disabled = false
-		move_coll.disabled = true
-		print("move coll")
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		jump_sfx.play()
+
+	# 2. Movement Logic
 	var direction := Input.get_axis("mleft", "mright")
 	
 	if direction < 0:
-		animated_sprite_2d.flip_h = true
-	elif direction > 0:
 		animated_sprite_2d.flip_h = false
+	elif direction > 0:
+		animated_sprite_2d.flip_h = true
 		
-	# ربط الأنيميشن بأشكال التصادم
-	if direction == 0: 
-		animated_sprite_2d.play("idle")
-		idle_coll.disabled = false
-		move_coll.disabled = true
-		print("idle coll")
-	else:
-		animated_sprite_2d.play("move")
-		idle_coll.disabled = true
-		move_coll.disabled = false
-		print("move coll")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	# 3. Animation and Collision State Machine
+	if not is_on_floor():
+		animated_sprite_2d.play("jump")
+	else:
+		if direction == 0: 
+			animated_sprite_2d.play("idle")
+			if idle_coll.disabled:
+				idle_coll.set_deferred("disabled", false)
+				move_coll.set_deferred("disabled", true)
+		else:
+			animated_sprite_2d.play("move")
+			if move_coll.disabled:
+				idle_coll.set_deferred("disabled", true)
+				move_coll.set_deferred("disabled", false)
+				
+	# -------------------------------------------------------------
+	# 4. Catch Logic 
+	# -------------------------------------------------------------؟
+	if Input.is_action_just_pressed("catch") and can_catch == true:
+		if not catch_sfx.playing:
+			catch_sfx.play() 
+		can_catch = false
+		catch_cooldown.start(.55) 
+		var caught_the_ball = false
+		
+		var overlapping_bodies = hitbox_idle.get_overlapping_bodies() + hitbox_move.get_overlapping_bodies()
+		for body in overlapping_bodies:
+			if body.is_in_group("ball"):
+				caught_the_ball = true
+				held_ball = body
+				break 
+		if caught_the_ball:
+			GameManger.add_score()
+			held_ball.set_deferred("freeze", true)
+			print("sisi")
+
+		else:
+			GameManger.add_misses() 
+
+	# -------------------------------------------------------------
+	# تحديث موقع الكرة الممسوكة لتتبع اللاعب
+	if held_ball and is_instance_valid(held_ball):
+		# يمكنك إضافة أرقام لتعديل مكان الكرة، مثلا وضعها عند فم اللاعب
+		held_ball.global_position = global_position
+	var was_falling: bool = not is_on_floor() and velocity.y > 0.0
 	
 	move_and_slide()
+	
+	if was_falling and is_on_floor():
+		landing_sfx.play()
+
+func _on_catch_cooldown_timeout() -> void:
+	can_catch = true
+	if held_ball and is_instance_valid(held_ball):
+		# 1. فك التجميد
+		held_ball.set_deferred("freeze", false)
+		
+		# 2. البحث عن كل الأعداء في مجموعة thrower
+		var throwers = get_tree().get_nodes_in_group("thrower")
+		if throwers.size() > 0:
+			# اختيار واحد عشوائي لرمي الكرة إليه
+			var random_thrower = throwers.pick_random()
+			
+			# التأكد أن الكرة بها دالة الرمي
+			if held_ball.has_method("throw"):
+				held_ball.throw(random_thrower.global_position)
+		
+		# 3. تفريغ المتغير استعداداً للإمساك بكرة جديدة
+		held_ball = null
