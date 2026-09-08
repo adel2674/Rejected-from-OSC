6182fb247ddcf1169ea801a6469c42b0c13867e5
Abdelrahman Medhat <AboodisGit@gmail.com>
2026-09-08 06:06:01 +0300

    add thrower movement, camera move, edit ui

--- game-jam/Scripts/thrower.gd
+++ game-jam/Scripts/thrower.gd
@@ -1,26 +1,32 @@
-
 extends CharacterBody2D
-@onready var ball: RigidBody2D = $"../ball"
-@onready var ani: AnimatedSprite2D = $AnimatedSprite2D
 
+@onready var ani: AnimatedSprite2D = $AnimatedSprite2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-
 @export var target_character: CharacterBody2D
 
-func throw():
+# --- NEW: Movement limits for the thrower ---
+@export var min_x: float = 0.0
+@export var max_x: float = 100.0
+
+func throw(ball: RigidBody2D):
 	ball.throw(target_character.global_position)
 	ani.play("throw")
 	$pop_sound.play()
+	
+	# --- NEW: Move to a random X position immediately after throwing ---
+	var random_x = randf_range(min_x, max_x)
+	var tween = get_tree().create_tween()
+	# Slide smoothly to the new X position over 0.4 seconds
+	tween.tween_property(self, "global_position:x", random_x, 0.4).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+	# ------------------------------------------------------------------
+	
 	await ani.animation_finished
 	ani.play("default")
 		
 		
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("ball"):
-		throw()
-		
-		
-	
+		throw(body)
